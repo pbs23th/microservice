cb64999db4ec417bcba5ea8cae2aa6f46182bccc
pbs23th <pbs23th1111Wgmail.com>
2022-04-28 19:01:30 +0900

    update views funtion

--- user_order/views.py
+++ user_order/views.py
@@ -1,35 +1,64 @@
-from rest_framework import viewsets
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Shop, Order
+from .serializers import ShopSerializer, OrderSerializer
 
 class ShopViewSet(viewsets.ViewSet):
     def list(self, request): #/api/shop
-        pass
+        shops = Shop.objects.all()
+        serializer = ShopSerializer(shops, many=True)
+        return Response(serializer.data)
+
 
     def create(self, request): #/api/shop
-        pass
+        serializer = ShopSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None): #/api/shop/<str:idx>
-        pass
+        shop = Shop.objects.get(id=pk)
+        serializer = ShopSerializer(shop)
+        return Response(serializer.data)
 
     def update(self, request, pk=None): #/api/shop/<str:idx>
-        pass
+        shop = Shop.objects.get(id=pk)
+        serializer = ShopSerializer(instance=shop, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy (self, request, pk=None): #/api/shop/<str:idx>
-        pass
+        shop = Shop.objects.get(id=pk)
+        shop.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class OrderViewSet(viewsets.ViewSet):
-    def list(self, request): #/api/Order
-        pass
+    def list(self, request):  # /api/shop
+        shops = Order.objects.all()
+        serializer = ShopSerializer(shops, many=True)
+        return Response(serializer.data)
 
-    def create(self, request): #/api/Order
-        pass
+    def create(self, request):  # /api/shop
+        serializer = OrderSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def retrieve(self, request, pk=None): #/api/Order/<str:idx>
-        pass
+    def retrieve(self, request, pk=None):  # /api/shop/<str:idx>
+        order = Order.objects.get(id=pk)
+        serializer = ShopSerializer(order)
+        return Response(serializer.data)
 
-    def update(self, request, pk=None): #/api/Order/<str:idx>
-        pass
+    def update(self, request, pk=None):  # /api/shop/<str:idx>
+        order = Order.objects.get(id=pk)
+        serializer = ShopSerializer(instance=order, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-    def destroy (self, request, pk=None): #/api/Order/<str:idx>
-        pass
+    def destroy(self, request, pk=None):  # /api/shop/<str:idx>
+        order = Order.objects.get(id=pk)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
